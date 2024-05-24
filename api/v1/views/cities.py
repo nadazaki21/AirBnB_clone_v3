8b4c4d35edcc8ad26b4db8051564500e0a761000
nadazkai21 <nadazaki019@gmail.com>
2024-05-25 01:02:12 +0300

    fixing errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -60,7 +60,7 @@ def create_city(state_id):
     city = City(**data)
     storage.new(city)
     storage.save()
-    return jsonify(city.to_dict()), 201
+    return json.dumps(city.to_dict(),indent=1), 201
 
 @app_views.route('/cities/<city_id>', strict_slashes=False, methods=['PUT'])
 def update_city(city_id):
