efc73c24080d32eb3e3cf7b1ae84b40600a60bcc
nadazkai21 <nadazaki019@gmail.com>
2024-05-25 01:24:15 +0300

    fixing errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -18,7 +18,7 @@ def return_cities_of_states(state_id):
         city_list = []
         for city in state.cities:
             city_list.append(city.to_dict())
-        return json.dumps(city_list, indent=2) +'\n'
+        return json.dumps(sorted(city_list), indent=2) +'\n'
     
 @app_views.route('/cities/<city_id>', strict_slashes=False , methods=['GET'])
 def return_city(city_id):
