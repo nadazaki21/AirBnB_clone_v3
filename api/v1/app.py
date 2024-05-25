11034261b21fd6a2a9e5eb4b74524e44e75ea400
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-05-25 19:47:20 +0300

    fix app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,5 +30,5 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", 5000))
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
