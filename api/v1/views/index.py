a7aec1b6b4e93155dd75e0d4cd697620676c68f7
nadazkai21 <nadazaki019@gmail.com>
2024-05-25 19:09:54 +0300

    task 4 finished

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,13 +8,22 @@ from models import storage
 from models.engine.db_storage import classes
 import json
 
-@app_views.route('/status', strict_slashes=False)
+
+@app_views.route("/status", strict_slashes=False)
 def return_status():
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', strict_slashes=False)
+
+@app_views.route("/stats", strict_slashes=False)
 def return_stats():
-    objects_perclass_count = {'amenities': 0, 'cities': 0, 'places': 0, 'reviews': 0, 'states': 0, 'users': 0}
+    objects_perclass_count = {
+        "amenities": 0,
+        "cities": 0,
+        "places": 0,
+        "reviews": 0,
+        "states": 0,
+        "users": 0,
+    }
     for key, value in zip(objects_perclass_count.keys(), classes.values()):
         objects_perclass_count[key] = storage.count(value)
-    return (json.dumps(objects_perclass_count, indent=2)) + '\n'
+    return (json.dumps(objects_perclass_count, indent=2)) + "\n"
