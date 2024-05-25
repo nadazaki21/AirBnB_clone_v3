2af1fea470655fccfcf0b214b9a3eee5894f3a9d
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-05-25 20:30:29 +0300

    make_response app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Flask App """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -19,7 +19,7 @@ def not_found(error):
       404:
         description: a resource was not found
     """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
