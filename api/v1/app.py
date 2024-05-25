3cf9532f1f6a77ec5f4b87a74d30bc852f54c3aa
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-05-25 20:28:01 +0300

    add cors to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,13 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.errorhandler(404)
 def not_found(error):
