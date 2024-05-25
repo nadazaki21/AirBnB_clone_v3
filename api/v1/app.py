885f3bdc85b9216ab11e780d2a0bcb0f09aaa5e0
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-05-25 22:33:46 +0300

    edit cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
+
 @app.errorhandler(404)
 def not_found(error):
     """404 Error
