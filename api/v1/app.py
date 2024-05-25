2c7399bebc39a671539ce30e2593ae99d0dbe890
nadazkai21 <nadazaki019@gmail.com>
2024-05-25 19:08:55 +0300

    task 5 finished

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
+    """404 Error
     ---
     responses:
       404:
@@ -20,15 +21,14 @@ def not_found(error):
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
 
-
-
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv  ("HBNB_API_PORT", "5000")
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
