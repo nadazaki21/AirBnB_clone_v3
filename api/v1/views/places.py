9734bdba5375b26de7c333947964cf44bd5fa79a
nadazkai21 <nadazaki019@gmail.com>
2024-05-27 02:23:25 +0300

    15

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -127,9 +127,6 @@ def places_search():
                     if amenity.id not in data["amenities"]:
                         final_list.remove(item)
         
-        #remove duplicates
-        final_list = list(set(final_list))
-
     elif "cities" in data and data["cities"] != []:
         for city in data["cities"]:
             for place in storage.all(Place).values():
